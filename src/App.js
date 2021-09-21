7f0d7ddd9d9ca709f2e9eea36682aa9f8abd65f1
Falak Amin <falakamin2000@gmail.com>
2021-09-21 20:57:28 +0500

    Routing,tailwindcss configuration,designed Login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import  {BrowserRouter as Router,Route,Switch } from "react-router-dom"
+import {lazy } from "react";
+import { Suspense } from 'react';
+import * as ROUTES from './constants/routes';
+const Login = lazy(() => import('./pages/login'));
 
 export const App = () => {
   return (
-    <div>
-      
-    </div>
+    
+      <Router>
+        <Suspense fallback={<p> Loading............. </p>}>
+
+        <Switch>
+          hello
+          <Route path={ROUTES.LOGIN} component={Login} />
+         </Switch>
+        </Suspense>
+      </Router>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true
+  },
+  theme:{
+    fill: (theme) => ({
+      red: theme('colors.red.primary')
+    }),
+    colors:{
+      white:"#ffffff",
+      blue:{
+        medium:"#0095f9"
+      },
+      black: {
+        light: '#262626',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      },
+      red: {
+        primary: '#ed4956'
+      }
+      }
+    
+    }
+  }
+  
